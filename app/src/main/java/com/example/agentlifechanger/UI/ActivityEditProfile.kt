3b180f3981a0aaff29cd8c92e2c1f0aed1b6ea5c
usman-mazhar1 <muhammadusmankalyar1717@gmail.com>
2023-08-27 15:06:53 +0500

    new changes

--- app/src/main/java/com/example/agentlifechanger/UI/ActivityEditProfile.kt
+++ app/src/main/java/com/example/agentlifechanger/UI/ActivityEditProfile.kt
@@ -74,7 +74,7 @@ class ActivityEditProfile : AppCompatActivity() {
             /*val intent = Intent(this@ActivityEditProfile, MainActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
             startActivity(intent)*/
         }
-
+//toas
     }
 
     private fun update(){
